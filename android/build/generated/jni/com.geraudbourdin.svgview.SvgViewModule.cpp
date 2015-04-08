60f900a10e37074819fa4c3beb38e5a3284813d0
geraudbourdin <gtwin@bourdin.name>
2015-04-08 15:10:51 +0200

    updated lib.

---
+++ android/build/generated/jni/com.geraudbourdin.svgview.SvgViewModule.cpp
@@ -0,0 +1,117 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2011-2013 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+/** This code is generated, do not edit by hand. **/
+
+#include "com.geraudbourdin.svgview.SvgViewModule.h"
+
+#include "AndroidUtil.h"
+#include "EventEmitter.h"
+#include "JNIUtil.h"
+#include "JSException.h"
+#include "Proxy.h"
+#include "ProxyFactory.h"
+#include "TypeConverter.h"
+#include "V8Util.h"
+
+
+
+#include "com.geraudbourdin.svgview.ViewProxy.h"
+
+#include "org.appcelerator.kroll.KrollModule.h"
+
+#define TAG "SvgViewModule"
+
+using namespace v8;
+
+		namespace com {
+		namespace geraudbourdin {
+		namespace svgview {
+
+
+Persistent<FunctionTemplate> SvgViewModule::proxyTemplate = Persistent<FunctionTemplate>();
+jclass SvgViewModule::javaClass = NULL;
+
+SvgViewModule::SvgViewModule(jobject javaObject) : titanium::Proxy(javaObject)
+{
+}
+
+void SvgViewModule::bindProxy(Handle<Object> exports)
+{
+	if (proxyTemplate.IsEmpty()) {
+		getProxyTemplate();
+	}
+
+	// use symbol over string for efficiency
+	Handle<String> nameSymbol = String::NewSymbol("SvgView");
+
+	Local<Function> proxyConstructor = proxyTemplate->GetFunction();
+	Local<Object> moduleInstance = proxyConstructor->NewInstance();
+	exports->Set(nameSymbol, moduleInstance);
+}
+
+void SvgViewModule::dispose()
+{
+	LOGD(TAG, "dispose()");
+	if (!proxyTemplate.IsEmpty()) {
+		proxyTemplate.Dispose();
+		proxyTemplate = Persistent<FunctionTemplate>();
+	}
+
+	titanium::KrollModule::dispose();
+}
+
+Handle<FunctionTemplate> SvgViewModule::getProxyTemplate()
+{
+	if (!proxyTemplate.IsEmpty()) {
+		return proxyTemplate;
+	}
+
+	LOGD(TAG, "GetProxyTemplate");
+
+	javaClass = titanium::JNIUtil::findClass("com/geraudbourdin/svgview/SvgViewModule");
+	HandleScope scope;
+
+	// use symbol over string for efficiency
+	Handle<String> nameSymbol = String::NewSymbol("SvgView");
+
+	Handle<FunctionTemplate> t = titanium::Proxy::inheritProxyTemplate(
+		titanium::KrollModule::getProxyTemplate()
+, javaClass, nameSymbol);
+
+	proxyTemplate = Persistent<FunctionTemplate>::New(t);
+	proxyTemplate->Set(titanium::Proxy::inheritSymbol,
+		FunctionTemplate::New(titanium::Proxy::inherit<SvgViewModule>)->GetFunction());
+
+	titanium::ProxyFactory::registerProxyPair(javaClass, *proxyTemplate);
+
+	// Method bindings --------------------------------------------------------
+
+	Local<ObjectTemplate> prototypeTemplate = proxyTemplate->PrototypeTemplate();
+	Local<ObjectTemplate> instanceTemplate = proxyTemplate->InstanceTemplate();
+
+	// Delegate indexed property get and set to the Java proxy.
+	instanceTemplate->SetIndexedPropertyHandler(titanium::Proxy::getIndexedProperty,
+		titanium::Proxy::setIndexedProperty);
+
+	// Constants --------------------------------------------------------------
+
+	// Dynamic properties -----------------------------------------------------
+
+	// Accessors --------------------------------------------------------------
+
+	return proxyTemplate;
+}
+
+// Methods --------------------------------------------------------------------
+
+// Dynamic property accessors -------------------------------------------------
+
+
+		} // svgview
+		} // geraudbourdin
+		} // com
---
+++ android/build/generated/obj/local/armeabi-v7a/objs/com.geraudbourdin.svgview/com.geraudbourdin.svgview.SvgViewModule.o.d
@@ -0,0 +1,188 @@
+/var/folders/0v/slqzg7hs6l74393tj2x_glh80000gn/T//gtwin/svgview-generated/obj/local/armeabi-v7a/objs/com.geraudbourdin.svgview/com.geraudbourdin.svgview.SvgViewModule.o: \
+ jni/com.geraudbourdin.svgview.SvgViewModule.cpp \
+ jni/com.geraudbourdin.svgview.SvgViewModule.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/jni.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stdarg.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/_cprolog.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/features.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/user_config.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/compat.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/host.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/_system.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/_android.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/_epilog.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h \
+ /Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/Proxy.h \
+ /Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/v8.h \
+ /Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/v8stdint.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stddef.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stdio.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/../include/stdio.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/JavaObject.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/assert.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/../include/assert.h \
+ /Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/EventEmitter.h \
+ /Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/NativeObject.h \
+ /Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/AndroidUtil.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/android/log.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stdlib.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/../include/stdlib.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/string.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/../include/string.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/strings.h \
+ /Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/memory.h \
+ /Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/EventEmitter.h \
+ /Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/JNIUtil.h \
+ /Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/AndroidUtil.h \
+ /Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/JSException.h \
+ /Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/ProxyFactory.h \
+ /Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/TypeConverter.h \
+ /Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/V8Util.h \
+ jni/com.geraudbourdin.svgview.ViewProxy.h \
+ /Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/org.appcelerator.kroll.KrollModule.h \
+ /Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/Proxy.h
+
+jni/com.geraudbourdin.svgview.SvgViewModule.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/jni.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stdarg.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/_cprolog.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/features.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/user_config.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/compat.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/host.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/_system.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/_android.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/_epilog.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h:
+
+/Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/Proxy.h:
+
+/Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/v8.h:
+
+/Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/v8stdint.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stddef.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stdio.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/../include/stdio.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/JavaObject.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/assert.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/../include/assert.h:
+
+/Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/EventEmitter.h:
+
+/Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/NativeObject.h:
+
+/Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/AndroidUtil.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/android/log.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/stdlib.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/../include/stdlib.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/sources/cxx-stl/stlport/stlport/string.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/../include/string.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/Users/gtwin/Documents/Titanium_Studio_Workspace/android-ndk-r10c/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/EventEmitter.h:
+
+/Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/JNIUtil.h:
+
+/Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/AndroidUtil.h:
+
+/Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/JSException.h:
+
+/Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/ProxyFactory.h:
+
+/Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/TypeConverter.h:
+
+/Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/V8Util.h:
+
+jni/com.geraudbourdin.svgview.ViewProxy.h:
+
+/Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/org.appcelerator.kroll.KrollModule.h:
+
+/Users/gtwin/Library/Application\ Support/Titanium/mobilesdk/osx/3.5.1.GA/android/native/include/Proxy.h:
