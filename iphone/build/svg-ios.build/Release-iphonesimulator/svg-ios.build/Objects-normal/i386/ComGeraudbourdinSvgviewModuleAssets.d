293574582c21e254d2f8addb59909aa9d3d2494b
geraudbourdin <gtwin@bourdin.name>
2015-05-23 11:20:27 +0200

    get the last SVGKIT updates

---
+++ iphone/build/svg-ios.build/Release-iphonesimulator/svg-ios.build/Objects-normal/i386/ComGeraudbourdinSvgviewModuleAssets.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/gtwin/Desktop/TiSvgView/Module/iphone/Classes/ComGeraudbourdinSvgviewModuleAssets.m \
+  /Users/gtwin/Desktop/TiSvgView/Module/iphone/Classes/ComGeraudbourdinSvgviewModuleAssets.h
